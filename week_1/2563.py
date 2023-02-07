fb9ac733b86a93c487f9a583eb29d12d032993f4
donghoon <gns9541gns@naver.com>
2023-02-07 21:04:16 +0900

    230207

--- week_1/2563.py
+++ week_1/2563.py
@@ -1,6 +1,5 @@
 N = int(input())
 p = [list(map(int, input().split())) for _ in range(N)]
-# print(p)
 
 x = []
 y = []
@@ -11,29 +10,26 @@ for i in range(0, 100):
 paper = []
 for i in range(100):
     for j in range(100):
-        paper.append((x[i],y[j]))
+        paper.append((x[i],y[j]))        # 100칸짜리 도화지에 좌표찍음
 # print(paper)
 cnt = 0
 for j in range(len(p)):
     
     x1=[]
     y1=[]
-    for i in range(0, 10):
+    for i in range(0, 10):               # 색종이의 좌표 
         x1.append(p[j][0]+i)
         y1.append(p[j][1]+i)
 
-    # print(x1,y1)
     pf = []
-    for i in (x1):
+    for i in (x1):                  
         for jdx in y1:
-            pf.append((i,jdx))
-    # print(pf)
+            pf.append((i,jdx))            # 색종이 좌표를 찍음
 
-    # cnt = 0
     for idx in pf:
         if idx in paper:
             paper.remove(idx)
-            cnt += 1
+            cnt += 1                      # 도화지에 색종이 좌표 있음 빼버리기고 그만큼 수 세기
         elif idx not in paper:
             pass
         
