a5c3c3264f3901073c2b9805838a5f5c2e559aa9
donghoon <gns9541gns@naver.com>
2023-02-14 20:54:04 +0900

    230214

--- week2/10158개미.py
+++ week2/10158개미.py
@@ -2,8 +2,8 @@ w, h = map(int, input().split())  # 격자 크기 가로 세로 6 4 6 4
 p, q = map(int, input().split())  # 개미 시작 위치 4 1 5 3
 t = int(input()) # 개미 움직인 시간 8 4
 
-a = abs(t-(w-p)) # 6 3       시간 - (가로에서 시작위치 - 격자가로)
-b = abs(t-(h-q)) # 5 3
+a = abs(t-(w-p))        #시간 - (가로에서 시작위치 - 격자가로)
+b = abs(t-(h-q)) 
 a1 = int(a/w)
 a2 = a%w
 b1 = int(b/h)
