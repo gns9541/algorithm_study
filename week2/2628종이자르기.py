e2d3155578c4438776b27b76f5fb3f7ee8558509
donghoon <gns9541gns@naver.com>
2023-02-16 19:50:00 +0900

    230216

--- week2/2628종이자르기.py
+++ week2/2628종이자르기.py
@@ -2,24 +2,24 @@ g, s = map(int, input().split()) # 가로 세로 길이
 N = int(input()) # 칼로 자를 점선의 개수
 line = [list(map(int, input().split())) for _ in range(N)]
 
-gl = [[1,0],[1,g]]
-sl = [[0,0],[0,s]]
+gl = [[1,0],[1,g]] # 가로로 잘리는 부분 리스트로 (0이랑 g포함) 
+sl = [[0,0],[0,s]] # 세로도
 for lst in line:
     if lst[0] == 1:
-        gl.append(lst)
+        gl.append(lst)   
         gl.sort()
     elif lst[0] == 0:
         sl.append(lst)
         sl.sort()
 
-sero = []
+sero = []              
 garo = []
-for i in range(len(gl)-1):
+for i in range(len(gl)-1):       # 위에 잘리는 부분 드러있는 리스트에서 가로 길이 4개 뽑기
     garo.append(gl[i+1][1] - gl[i][1])
-for i in range(len(sl)-1):
+for i in range(len(sl)-1):       # 세로도
     sero.append(sl[i+1][1] - sl[i][1])
 
-final = []
+final = []                   # 뽑은 가로길이 랑 세로길이의 곱에서 가장 큰값 
 for i in garo:
     for j in sero:
         final.append(i*j)
