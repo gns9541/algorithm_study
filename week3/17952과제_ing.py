6f0697560e39044dfe2ce1ab322fe7a24cf72e82
donghoon <gns9541gns@naver.com>
2023-02-21 19:40:15 +0900

    .

--- week3/17952과제_ing.py
+++ week3/17952과제_ing.py
@@ -1,15 +1,22 @@
 N = int(input())
-time = N
-bit = [0]*N
-lst = [list(map(int, input().split())) for _ in range(N)]
-for i in range(N):
-    if lst[i][0] == 1:
-        bit[i] = lst[i][2]
-    
-    if bit[i] != 0:
-        bit[i] -= 1
-    time -= 1
-    print(time)
-    print(bit)
+stk = []
+final = 0
+# lst = [list(map(int, input().split())) for _ in range(N)]
+# for i in range(N):
+for _ in range(N):
+    lst = list(map(int, input().split()))
+    if lst[0] == 1:
+        if lst[2] == 1:
+            final += lst[1]
+        else:
+            stk.append([lst[1], lst[2]-1])
+    # print(stk)
+    if stk:
+        stk[-1][1] -=1
+        if stk[-1][1] == 0:
+            final += stk.pop()[0]
+    # print(stk)
+print(final)
+# 시간초과
 
 
